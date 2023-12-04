3d1a29594998283165f8e52e8b3511fb40ef88b6
KumaTea <KumaTea@outlook.com>
2023-12-04 21:38:01 +0800

    add handler

--- func/general.py
+++ func/general.py
@@ -1,6 +1,7 @@
 from func.avail import show_avail
 from func.limit import show_limit
 from func.lang.bash import command_bash
+from func.lang.build import command_build
 from func.lang.node import command_node
 from func.lang.perl import command_perl
 from func.lang.ruby import command_ruby
--- handlers/register.py
+++ handlers/register.py
@@ -13,6 +13,7 @@ def register_handlers():
     # group commands
     # langs
     eval_bot.add_handler(MessageHandler(command_bash, filters.command(LANG_CMDS['bash']) & filters.group))
+    eval_bot.add_handler(MessageHandler(command_build, filters.command(LANG_CMDS['build']) & filters.group))
     eval_bot.add_handler(MessageHandler(command_node, filters.command(LANG_CMDS['node']) & filters.group))
     eval_bot.add_handler(MessageHandler(command_perl, filters.command(LANG_CMDS['perl']) & filters.group))
     eval_bot.add_handler(MessageHandler(command_ruby, filters.command(LANG_CMDS['ruby']) & filters.group))
